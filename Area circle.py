dbf7bbabcf18f98acd866b735a1bb6c695fc859f
acvarshinee <104981722+acvarshinee@users.noreply.github.com>
2022-05-13 21:00:02 +0530

    Update and rename LICENSE to Area circle.py

---
+++ Area circle.py
@@ -0,0 +1,9 @@
+#area of circle 
+r=int(input("enter radius")
+area=3.14*r**2
+printf("area of circle:",area)
+
+#include
+f=input("enter the file name:")
+add=(f+".py)
+Printf("extension of file name:",add)
